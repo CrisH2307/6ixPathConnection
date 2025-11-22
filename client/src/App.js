193e12db40c7b4ad8a3094d24a2dc16a31471fd4
CrisH2307 <khangpicasso@gmail.com>
2025-11-21 21:42:59 -0500

    Update home.js, tailwindcss, UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,7 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import SimilarPeople from "./pages/SimilarPeople";
-import Home from "./pages/Home.js";
+import Home from "./pages/Home";
 
 function App() {
-  
-
-  return (
-    <>
-    <Routes>
-      <Route path="/similar" element={<SimilarPeople />} />
-      <Route path="/" element={<Home />} />
-    </Routes>
-    </>
-  );
+  return <Home />;
 }
 
 export default App;
